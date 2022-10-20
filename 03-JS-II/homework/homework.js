9d49b6e3b1bcb53f5276994ff45feee7377f3fcf
Leoluceromtz <leoluceromtz@gmail.com>
2022-10-20 00:33:23 -0500

    8thCommit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -197,7 +197,7 @@ function tablaDelSeis() {
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
   let tabla6 = [];
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i < 11; i++) {
     tabla6.push(6 * i);
   }
   return tabla6;
